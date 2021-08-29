9d8574f69e1ef709e1770d2f722a05ef70572e83
sweetautumndeer <amoulios@yahoo.com>
2021-08-29 15:34:37 -0700

    Difficulty now scales with gameDifficulty
    also the player cursor spawns in the middle of the face

--- Assets/Scripts/EtchASketch/EtchASketch_GridHandler.cs
+++ Assets/Scripts/EtchASketch/EtchASketch_GridHandler.cs
@@ -16,9 +16,10 @@ public class EtchASketch_GridHandler : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
-        InitPlayerTilemap();
-        InitTargetTilemap();
-        playerPos = new Vector3Int(0, 0, 0);
+        InitTilemap(targetTiles);
+        InitTilemap(playerTiles);
+        GameController.Instance.SetMaxTimer((4 - GameController.Instance.gameDifficulty) * 5);
+        playerPos = new Vector3Int(4, 4, 0);
     }
 
     // Update is called once per frame
@@ -54,49 +55,41 @@ public class EtchASketch_GridHandler : MonoBehaviour {
         }
         
     }
-    
-    void InitTargetTilemap() {
-        targetTiles.ClearAllTiles();
-        targetTiles.size = new Vector3Int(9, 9, 0);
-        targetTiles.ResizeBounds();
+
+    void InitTilemap(Tilemap tiles) {
+        tiles.ClearAllTiles();
+        tiles.size = new Vector3Int(9, 9, 0);
+        tiles.ResizeBounds();
         for (int x = 0; x < 9; ++x) {
             for (int y = 0; y < 9; ++y) {
-                targetTiles.SetTile(new Vector3Int(x, y, 0), white);
-            }
-        }
-
-        Vector3Int[] vectorArray = {new Vector3Int(1, 3, 0), new Vector3Int(2, 2, 0), 
-                                    new Vector3Int(3, 2, 0), new Vector3Int(4, 2, 0), 
-                                    new Vector3Int(5, 2, 0), new Vector3Int(6, 2, 0), 
-                                    new Vector3Int(7, 3, 0), new Vector3Int(2, 6, 0), 
-                                    new Vector3Int(6, 6, 0)};
-
-        for (int i = 0; i < vectorArray.Length; ++i) {
-            targetTiles.SetTile(vectorArray[i], black);
-        }
-    }
-
-    // Draw whitspace and choose a random black tile for the player to fill in(?)
-    void InitPlayerTilemap() {
-        playerTiles.ClearAllTiles();
-        playerTiles.size = new Vector3Int(9, 9, 0);
-        playerTiles.ResizeBounds();
-        for (int x = 0; x < 9; ++x) {
-            for (int y = 0; y < 9; ++y) {
-                playerTiles.SetTile(new Vector3Int(x, y, 0), white);
+                tiles.SetTile(new Vector3Int(x, y, 0), white);
             }
         }
 
         int rand = Random.Range(1, 9);
+        int rand2, rand3;
+        do {
+            rand2 = Random.Range(1, 9);
+        } while (rand2 == rand);
+        do {
+            rand3 = Random.Range(1, 9);
+        } while (rand3 == rand2 || rand3 == rand);
+
+        if (GameController.Instance.gameDifficulty < 3)
+            rand3 = rand;
+        if (GameController.Instance.gameDifficulty < 2)
+            rand2 = rand;
+
         Vector3Int[] vectorArray = {new Vector3Int(1, 3, 0), new Vector3Int(2, 2, 0), 
                                     new Vector3Int(3, 2, 0), new Vector3Int(4, 2, 0), 
                                     new Vector3Int(5, 2, 0), new Vector3Int(6, 2, 0), 
                                     new Vector3Int(7, 3, 0), new Vector3Int(2, 6, 0), 
                                     new Vector3Int(6, 6, 0)};
 
+        //Draw all tiles for targetTiles, but omit one for playerTiles
         for (int i = 0; i < vectorArray.Length; ++i) {
-            if (i != rand)
-                playerTiles.SetTile(vectorArray[i], black);
+            if ((i != rand && i != rand2 && i != rand3) || tiles == targetTiles)
+                tiles.SetTile(vectorArray[i], black);
         }
     }
 
